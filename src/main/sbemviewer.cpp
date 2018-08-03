4a9334cc25bf12fbfa27a4350b6e32fd9f1fc4a9
Daniel A. Wagenaar <daw@caltech.edu>
2018-08-02 21:21:35 -0700

    Now can open other servers

--- src/main/sbemviewer.cpp
+++ src/main/sbemviewer.cpp
@@ -6,17 +6,44 @@
 #include <QApplication>
 #include <QDebug>
 #include <QFileInfo>
+#include <QInputDialog>
+
 #include "Settings.h"
 #include "TileViewer.h"
 
+const QString defaultServer = "http://leechem.caltech.edu:9090";
+
+QString getServer(QString dflt) {
+  QString server = QInputDialog::getText(0, "SBEM Viewer", "Server name",
+                                         QLineEdit::Normal, dflt);
+  return server;
+}
+
 int main(int argc, char **argv) {
   Settings settings;
-  QString server = "http://leechem.caltech.edu:9090";
   bool cleanexit = settings.get("cleanexit", false).toBool();
+  QString dbfn = settings.get("database", "").toString();
+  QString srv0 = settings.get("server", defaultServer).toString();
+
   settings.set("cleanexit", false);
+  if (!cleanexit)
+    settings.set("database", "");
+
   QApplication app(argc, argv);
+
+  QString server = getServer(server);
+  if (!server.startsWith("http://") && !server.startsWith("https://"))
+    server = "http://" + server;
+  if (server != srv0) {
+    // database likely refers to specific server, so...
+    cleanexit = false;
+    dbfn = "";
+    settings.set("database", "");
+  }
+  
   TileCache cache(server);
   ServerInfo info(server);
+
   MainWindow mw(&cache, &info);
   mw.show();
   if (cleanexit) {
@@ -25,7 +52,6 @@ int main(int argc, char **argv) {
                     settings.get("y", tv->y()).toInt(),
                     settings.get("z", tv->z()).toInt());
     tv->setScale(settings.get("a", tv->scale()).toInt());
-    QString dbfn = settings.get("database", "").toString();
     if (!dbfn.isEmpty() && QFileInfo::exists(dbfn))
       mw.openDB(dbfn);
   }
@@ -38,5 +64,6 @@ int main(int argc, char **argv) {
   settings.set("z", tv->z());
   settings.set("a", tv->scale());
   settings.set("cleanexit", true);
+  settings.set("server", server);
   return 0;
 }
